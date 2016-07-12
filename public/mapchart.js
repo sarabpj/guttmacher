54e3a82dca362eed1a54d85983f5dc365e122564
sarabpj <sjay02@yahoo.com>
2016-07-12 10:05:41 -0700

    first graph appending

--- public/mapchart.js
+++ public/mapchart.js
@@ -16,23 +16,78 @@ $("#container").click(function(e){
     var clickedState = temp.slice(0, temp.length - 1).replace(/ /g,"_")
 
     $('#statename').text(clickedState)
-    graph1(clickedState)
-
+    pieData(clickedState)
+    // chart([54,23,10])
 });
 
 
 //gives back the data i need for the first
-function graph1(clickedState){
+function pieData(clickedState){
+  var arr = []
   d3.json('v1/table_1.2', function(data){
-    var arr = []
     var stateData = data.filter(val =>{
           return val['US_State'] === clickedState;
     })[0]
-    arr.push(parseInt(stateData.NoB_15_19.replace(/,|_|\[.*\]/g,'')), parseInt(stateData.NoA_15_19.replace(/,|_|\[.*\]/g,'')), parseInt(stateData.NoF_15_19.replace(/,|_|\[.*\]/g,'')) )    
+ 
+    //data for chart
+    arr.push(parseFloat(stateData.NoB_15_19.replace(/,|_|\[.*\]/g,'')), parseFloat(stateData.NoA_15_19.replace(/,|_|\[.*\]/g,'')), parseFloat(stateData.NoF_15_19.replace(/,|_|\[.*\]/g,'')) )    
     console.log(arr)
-    return(arr)
+    //run the chart function which appends a graph...
+    chart(arr);
+
   })
+
+
 }
+
+
+function chart(arr){
+ 
+  var data = arr;
+  var r =50;
+  var color = d3.scale.category10()
+
+  //canvas
+  var canvas = d3.select('body').append('svg')
+    .attr('width', 100)
+    .attr('height', 100);
+
+  //store visualization
+  var group = canvas.append('g')
+    .attr('transform', 'translate(50,50)')
+
+  //arc path generator
+  var arc = d3.svg.arc()
+    .innerRadius(0)
+    .outerRadius(r);
+
+  //layouts are built into d3, takes our data
+  var pie = d3.layout.pie()
+    .value(function(d){return d;})
+
+  var arcs = group.selectAll(".arc")
+    .data(pie(data))
+    .enter()
+    .append('g')
+    .attr('class', 'arc')
+
+  arcs.append('path')
+    .attr('d', arc)
+    .attr('fill', function(d){
+      return color(d.data);
+    })
+
+  arcs.append('text')
+    .attr('transform', function(d){return "translate (" + arc.centroid(d) + ")";})
+    .attr('text-anchor', 'middle')
+    .attr('font-size', '.8em')
+    .text(function(d){ return d.data})
+    
+ 
+}
+
+
+
 
 
 
